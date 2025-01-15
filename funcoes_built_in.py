52d1a0a9bcf882da92a57ffc2064b4a6b6b11af9
Sávio <saviopf22@gmail.com>
2025-01-15 12:54:06 -0300

    criação funções built-in

---
+++ funcoes_built_in.py
@@ -0,0 +1,15 @@
+print("Funções built-in")
+
+print("abs()", abs(-9.2))
+
+print("min()", min(56, 2, 4, 10))
+
+print("max()", max(56, 2, 4, 10))
+
+print("sum()", sum([56, 2, 4, 10]))
+
+print("pow()", pow(2, 3))
+
+print("input()", input("digite algo: "))
+
+print("help()", help(input))
