33ab52ae0d7cebebe19044d57b8be7644256353b
Sávio <saviopf22@gmail.com>
2025-01-15 12:04:57 -0300

    criação operadores matematicos

---
+++ operadores.py
@@ -0,0 +1,23 @@
+print("operadores matematicos")
+
+print("inteiro:", 1)
+
+print("decimal:", 1.5)
+
+print("tupla -> 1, 5:", 1, 5)
+
+print("soma -> 1 + 1:", 1 + 1)
+
+print("subtracao -> 2 - 1:", 2 - 1)
+
+print("multiplicacao -> 4 * 4:", 4 * 4)
+
+print("divisão -> 10 / 2:", 10 / 2)
+
+print("divisão (parte inteira) -> 10 // 2:", 10 // 2)
+
+print("divisão (parte inteira) -> 10 // 3:", 10 // 3)
+
+print("resto da divisão -> 10 % 2:", 10 % 2)
+
+print("resto da divisão -> 10 % 3:", 10 % 3)
