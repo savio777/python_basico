65425155dc185b894ef015faa2acf55c6c9f1c43
Sávio <saviopf22@gmail.com>
2025-01-16 16:33:44 -0300

    add espaços exercicios4

--- exercicios4_funcoes.py
+++ exercicios4_funcoes.py
@@ -1,4 +1,4 @@
-print("Funções Exercícios")
+print("Funções Exercícios\n")
 
 
 def response1(number=5):
@@ -31,13 +31,20 @@ print(
 )
 response1()
 
+print("\n", "- " * 50)
+
 print(
     "\n2. Escreva uma função que receba como parâmetro uma temperatura em Celsius e converta para Fahrenheit."
 )
 response2()
+
+print("\n", "- " * 50)
+
 
 print(
     "\n3. Escreva uma função que receba dois parâmetros. O primeiro será uma palavra, se ela for quadrado deverá calcular o quadrado do número passado como segundo parâmetro. Se a palavra for cubo deverá realizar o calculo do cubo do número passado."
 )
 response3("quadrado", 3)
 response3("cubo", 3)
+
+print("\n", "- " * 50)
