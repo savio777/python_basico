8efafc4fd2059647c6c4e9359adc61834feafda1
Sávio <saviopf22@gmail.com>
2025-01-15 23:21:14 -0300

    criação if e readme

---
+++ if.py
@@ -0,0 +1,19 @@
+print("Instrução If")
+
+number = 10
+
+if number == 10:
+    print("igual a 10")
+
+
+if number > 10:
+    print("maior que 10")
+elif number < 10:
+    print("menor que 10")
+else:
+    print("igual a 10")
+
+if number >= 10:
+    print("maior ou igual a 10")
+elif number <= 10:
+    print("menor ou igual a 10")
